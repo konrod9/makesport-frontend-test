702f90a88fabfffe59db13f83e6572be6b383581
Rodion.Kondrashin <rodion.kondrashin17@gmail.com>
2026-06-13 22:24:39 +0700

    add dist

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-};
-
-module.exports = {
-  allowedDevOrigins: ["127.0.0.1", "0.0.0.0"],
+  output: "export",
+  distDir: "dist",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
